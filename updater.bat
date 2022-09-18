c5772174cd6e0b5989951c6e5b14bf1760208976
wahyuu2821 <63901359+wahyuu2821@users.noreply.github.com>
2022-09-18 12:59:07 +0700

    Update updater.bat

--- updater.bat
+++ updater.bat
@@ -1,26 +1,29 @@
 @ECHO OFF
 setlocal EnableDelayedExpansion
 set "curver=0.1.1"
+set "curfol=%cd%\adb_batch"
+cd adb_batch
 attrib -h fconfig.bat
 call fconfig.bat
 attrib +h fconfig.bat
 
 IF NOT EXIST "version.bat" (
-    curl --silent --output %cd%\version.bat --url https://raw.githubusercontent.com/wahyuu2821/ADB_Batch/main/version.bat?token=GHSAT0AAAAAABYSTZID2CXGNW7WU4MZGEXOYZGU4FQ
+    curl --silent --output %curfol%\version.bat --url https://raw.githubusercontent.com/wahyuu2821/ADB_Batch/main/version.bat?token=GHSAT0AAAAAABYSTZID2CXGNW7WU4MZGEXOYZGU4FQ
 )
 IF NOT EXIST "version.bat" GOTO :EOF
 call version.bat
 
 IF %curver%==%version% (
     IF NOT EXIST "adb_batch.bat" (
-        curl --silent --output %cd%\adb_batch.bat --url https://raw.githubusercontent.com/wahyuu2821/ADB_Batch/main/adb_batch.bat?token=GHSAT0AAAAAABYSTZICQO2V3XDIYKPTJIWUYZGU4BA
+        curl --silent --output %curfol%\adb_batch.bat --url https://raw.githubusercontent.com/wahyuu2821/ADB_Batch/main/adb_batch.bat?token=GHSAT0AAAAAABYSTZICQO2V3XDIYKPTJIWUYZGU4BA
     ) ELSE (
-        call adb_batch.bat
+        start adb_batch.bat
     )
 ) ELSE (
-    curl --silent --output %cd%\adb_batch.bat --url https://raw.githubusercontent.com/wahyuu2821/ADB_Batch/main/adb_batch.bat?token=GHSAT0AAAAAABYSTZICQO2V3XDIYKPTJIWUYZGU4BA
-    curl --silent --output %cd%\updater.bat --url https://raw.githubusercontent.com/wahyuu2821/ADB_Batch/main/updater.bat?token=GHSAT0AAAAAABYSTZID743UQJXDV3KBFR5WYZGU4EQ
-    call %batchfirst%/test.bat
+    curl --silent --output %curfol%\adb_batch.bat --url https://raw.githubusercontent.com/wahyuu2821/ADB_Batch/main/adb_batch.bat?token=GHSAT0AAAAAABYSTZICQO2V3XDIYKPTJIWUYZGU4BA
+    curl --silent --output %curfol%\updater.bat --url https://raw.githubusercontent.com/wahyuu2821/ADB_Batch/main/updater.bat?token=GHSAT0AAAAAABYSTZID743UQJXDV3KBFR5WYZGU4EQ
+    cd %batchfirst%
+    start test.bat
 )
 
 del version.bat
