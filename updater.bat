ac64f4a19efc1a96a83dbf96ec0a1c77938339cd
wahyuu2821 <63901359+wahyuu2821@users.noreply.github.com>
2022-09-18 20:41:16 +0700

    Update updater.bat

--- updater.bat
+++ updater.bat
@@ -9,11 +9,18 @@ attrib +h fconfig.bat
 
 IF NOT EXIST "version.bat" (
     curl --silent --output %curfol%\version.bat --url https://raw.githubusercontent.com/wahyuu2821/ADB_Batch/main/version.bat?token=GHSAT0AAAAAABYSTZID2CXGNW7WU4MZGEXOYZGU4FQ
+) ELSE (
+    GOTO :adbb
 )
 timeout /t 1 > nul
 IF NOT EXIST "version.bat" GOTO :end
 call version.bat
 
+:end
+del version.bat
+exit
+
+:adbb
 IF %curver%==%version% (
     IF NOT EXIST "adb_batch.bat" (
         curl --silent --output %curfol%\adb_batch.bat --url https://raw.githubusercontent.com/wahyuu2821/ADB_Batch/main/adb_batch.bat?token=GHSAT0AAAAAABYSTZICQO2V3XDIYKPTJIWUYZGU4BA
@@ -29,7 +36,4 @@ IF %curver%==%version% (
     timeout /t 2 > nul
     start test.bat
 )
-
-:end
-del version.bat
-exit
+GOTO :EOF
